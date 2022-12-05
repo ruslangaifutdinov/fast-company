def23322c178eb0246fb3ea524a172d161a650c8
Ruslan Gaifutdinov <ruslangaifutdinov@Ruslans-MacBook-Pro.local>
2022-12-05 21:50:47 +0300

    Refactoring users and usersTable

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { paginate } from "../utils/paginate";
 import Pagination from "./pagination";
-import User from "./user";
 import api from "../api";
 import PropTypes from "prop-types";
 import GroupList from "./groupList";
 import SearchStatus from "./searchStatus";
+import UsersTable from "./usersTable";
 
 const Users = ({ users, ...rest }) => {
     const pageSize = 4;
@@ -64,26 +64,7 @@ const Users = ({ users, ...rest }) => {
             )}
             <div className="d-flex flex-column">
                 <SearchStatus length={count} />
-                {count > 0 && (
-                    <table className="table align-middle">
-                        <thead>
-                            <tr>
-                                <th scope="col">Имя</th>
-                                <th scope="col">Качества</th>
-                                <th scope="col">Профессия</th>
-                                <th scope="col">Встретился, раз</th>
-                                <th scope="col">Оценка</th>
-                                <th scope="col">Избранное</th>
-                                <th scope="col"></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {userCrop.map((user) => (
-                                <User key={user._id} {...rest} {...user} />
-                            ))}
-                        </tbody>
-                    </table>
-                )}
+                {count > 0 && <UsersTable users={userCrop} {...rest} />}
                 <div className="d-flex justify-content-center">
                     <Pagination
                         itemsCount={count}
